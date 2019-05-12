c14e56350ffe723df0d5710f5a4552cf95e19bb2
Isaac Bhasme <isvicbhasme@live.com>
2019-05-12 23:17:28 +0530

    Initial commit

---
+++ src/main/java/com/isaac/eventful/EventfulApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.eventful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventfulApplication.class, args);
+    }
+
+}
